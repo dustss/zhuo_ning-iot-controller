36849f85d393d3697dbcc2cb006f18f64c435d1a
dustss <oneisoneonlyone@foxmail.com>
2016-04-02 12:58:09 +0800

    2016-04-02-01   优化了目录结构

---
+++ app/src/main/java/zhuoning/youthlife/cn/zhuoning/cn/youthlife/zhuoning/vo/URL.java
@@ -0,0 +1,13 @@
+package zhuoning.youthlife.cn.zhuoning.cn.youthlife.zhuoning.vo;
+
+/**
+ * Created by dusts on 2016/4/2.
+ */
+public class URL {
+
+    public static final String URL_BASE = "http://api.zhuoningiot.com/service.asmx/";
+    public static final String URL_REG = URL_BASE+"";
+    public static final String URL_LOGIN = URL_BASE+"";
+    public static final String URL_CHANGEPASS = URL_BASE+"";
+
+}
